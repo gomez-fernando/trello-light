9233b85a6e66baeec2ebf33b67a5e174749af8d4
gomez-fernando <ferdgomez@hotmail.es>
2022-08-02 12:05:50 +0100

    Define the MOVE_LIST action

--- src/state/appStateReducer.ts
+++ src/state/appStateReducer.ts
@@ -1,5 +1,5 @@
 import { nanoid } from "nanoid";
-import { findItemIndexById } from "../utils/arrayUtils";
+import { findItemIndexById, moveItem } from "../utils/arrayUtils";
 import { Action } from "./actions";
 
 export type Task = {
@@ -31,7 +31,6 @@ export const appStateReducer = (
       break;
     }
     case "ADD_TASK": {
-      console.log('add tassk');
       const { text, listId } = action.payload;
       const targetListIndex = findItemIndexById(draft.lists, listId);
 
@@ -39,6 +38,17 @@ export const appStateReducer = (
         id: nanoid(),
         text,
       });
+      break;
+    }
+    case 'MOVE_LIST': {
+      const {draggedId, hoverId} = action.payload;
+      const dragIndex = findItemIndexById(draft.lists, draggedId);
+      const hoverIndex = findItemIndexById(draft.lists, hoverId);
+      draft.lists = moveItem(draft.lists, dragIndex, hoverIndex);
+      break;
+
+    }
+    default: {
       break;
     }
   }
